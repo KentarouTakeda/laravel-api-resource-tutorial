778386b474f67315523b11e904b50ddf276d955c
武田 憲太郎 <takeda@youmind.jp>
2025-10-06 17:10:11 +0900

    controller: 1件表示`show`の実装

--- app/Http/Controllers/TodoController.php
+++ app/Http/Controllers/TodoController.php
@@ -29,7 +29,7 @@ class TodoController extends Controller
      */
     public function show(Todo $todo)
     {
-        //
+        return ['data' => $todo];
     }
 
     /**
--- tests/Feature/Http/Controllers/TodoControllerTest.php
+++ tests/Feature/Http/Controllers/TodoControllerTest.php
@@ -2,19 +2,36 @@
 
 namespace Tests\Feature\Http\Controllers;
 
+use App\Models\Todo;
+use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class TodoControllerTest extends TestCase
 {
-    /**
-     * A basic feature test example.
-     */
-    public function test_example(): void
-    {
-        $response = $this->get('/');
+    use RefreshDatabase;
 
-        $response->assertStatus(200);
+    private User $user;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        $this->user = User::factory()->create();
+
+        $this->actingAs($this->user);
+    }
+
+    public function test_show(): void
+    {
+        Todo::factory()
+            ->for($this->user)
+            ->create(['id' => 42, 'title' => 'foo']);
+
+        $this->json('GET', 'todos/42')
+            ->assertStatus(200)
+            ->assertJsonPath('data.id', 42)
+            ->assertJsonPath('data.title', 'foo');
     }
 }
